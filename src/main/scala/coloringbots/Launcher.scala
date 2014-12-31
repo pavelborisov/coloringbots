638a456d9109e0031b209b3649546f338834c492
pavelborisov <pborisov@naumen.ru>
2014-12-31 11:54:17 +0300

    white bot

--- src/main/scala/coloringbots/Launcher.scala
+++ src/main/scala/coloringbots/Launcher.scala
@@ -1,6 +1,6 @@
 package coloringbots
 
-import coloringbots.bots.{ImmortalBot, SeqBotV2, SeqBot, RandomBot}
+import coloringbots.bots._
 
 /**
  * User: rgordeev
@@ -9,13 +9,14 @@ import coloringbots.bots.{ImmortalBot, SeqBotV2, SeqBot, RandomBot}
  */
 object Launcher extends App{
   private val timer = new TimeZombi
-  Game(Coord(9, 9), 50)
+  Game(Coord(9, 9), 10)
     .register(ImmortalBot("blue"))
     .register(RandomBot("red"))
     .register(RandomBot("green"))
     .register(SeqBot("yellow"))
     .register(SeqBotV2("orange"))
     .register(ChampionBot("pink"))
+    .register(RapidBot("white"))
     .register(PrintZombi)
     .register(timer)
     .play
---
+++ src/main/scala/coloringbots/bots/RapidBot.scala
@@ -0,0 +1,66 @@
+package coloringbots.bots
+
+import java.util
+
+import coloringbots._
+
+import scala.util.Random
+
+/**
+ * Created by ����� on 31.12.2014.
+ */
+case class RapidBot(val color: String) extends Bot {
+  val random = new Random
+  val cellMap:collection.mutable.ArrayBuffer[(Int,Int)]= collection.mutable.ArrayBuffer.empty[(Int,Int)]
+
+  lazy val init = {
+    var i:Int = 0
+    while( i < field.size.x ) {
+      var j:Int = 0
+      while( j < field.size.y ) {
+        field.get(Coord(i, j)) match {
+          case Some(cell) if cell.isEmpty =>
+            cellMap += ((i, j))
+          case _ =>
+        }
+        j=j+1
+      }
+      i=i+1
+    }
+  }
+
+  override def nextTurn: Turn = {
+    if( cellMap.isEmpty) init
+
+    val (x:Int,y:Int) = cellMap( random.nextInt( cellMap.size ) )
+
+    val t = this -> (x,y)
+    if( !t.validate )
+    {
+      (0 to field.size.y).foreach {y=>
+        (0 to field.size.x).foreach{ x =>
+          val t: Turn = this ->(x, y)
+          if (t.validate)
+            return t
+        }
+      }
+    }
+
+    t
+  }
+
+  override def notify(cell: Cell) = {
+    traverse( cell )
+    cell.neighbours.foreach( traverse _ )
+
+    def traverse(cell:Cell) = {
+      if ( !cell.whose.contains(this) ) {
+        val turn = Turn(this, cell)
+        if (turn.validate)
+          cellMap += ((cell.coord.x, cell.coord.y))
+      }
+    }
+  }
+
+  override var field: Field = null
+}
